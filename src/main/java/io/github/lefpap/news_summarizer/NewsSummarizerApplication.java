f338ab8feba0bc3a224b839d4ba971bdf05f67fd
lefpap <lolopap21@gmail.com>
2025-05-08 16:55:07 +0300

    initial commit

---
+++ src/main/java/io/github/lefpap/news_summarizer/NewsSummarizerApplication.java
@@ -0,0 +1,13 @@
+package io.github.lefpap.news_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lefpap/news_summarizer/NewsSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lefpap.news_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
